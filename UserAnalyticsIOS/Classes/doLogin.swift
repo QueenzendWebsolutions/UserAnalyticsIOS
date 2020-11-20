c3b89b77ba86e75bdca53e6e70610853f5f60541
QueenzendWebsolutions <71247084+QueenzendWebsolutions@users.noreply.github.com>
2020-11-20 16:26:37 +0530

    Add files via upload

---
+++ UserAnalyticsIOS/Classes/doLogin.swift
@@ -0,0 +1,44 @@
+//
+//  doLogin.swift
+//  UserAnalyticsIOS
+//
+//  Created by Queenzend Web Solution on 20/11/20.
+//
+
+import Foundation
+//import Alamofire
+
+func doLogin(){
+                                   print("username and password Info");
+                                   
+                            /*   let url = "https://k2key.in/marketing_plateform_CI/Webservice/appUserLoginNew";
+
+                                   AF.request(URL.init(string: url)!, method: .post, parameters: params, encoding: JSONEncoding.default,headers: [:])
+               
+                                       .responseData { response in
+                                          guard let data = response.data else { return }
+                                          print(response.result)
+                                          print(String(data: data, encoding: .utf8)!)
+                                          let json = JSON(data)
+                                           print(json)
+                                             let res_data = json["res_data"]
+                                             print(res_data)
+
+                                             let res_code = json["res_code"]
+                                             print(res_code)
+                                          if(res_code == 1){
+                                            self.isActive = true
+                                                          self.message = "User logged successfully"
+                                                          self.showAlert = true
+                                                         
+                                                          return
+                                                      }
+                                                      else{
+                                                      self.attemptingLogin = false
+                                                          self.message = "Login Failed"
+                                                          self.showAlert = true
+                                                         
+                                                      }
+                                       }*/
+                           }
+                
